8e2a42db28d571e708a26d19e74b04a976048739
SATTIBABUCHITTALA <57794381+SATTIBABUCHITTALA@users.noreply.github.com>
2021-07-23 09:57:09 +0530

    Add files via upload

---
+++ StudentGradeDetails.java
@@ -0,0 +1,13 @@
+package com.aarya.student;
+
+public class StudentGradeDetails {
+
+	StudentInfo studentInfo=new StudentInfo();
+	
+public void getStudentGradeDetails() {
+
+	studentInfo.getStudentInfo();
+}
+	
+	
+}
---
+++ StudentInfo.java
@@ -0,0 +1,13 @@
+package com.aarya.student;
+
+ public class StudentInfo {
+
+	
+	public void getStudentInfo() {
+		System.out.println("Student Details will be Provided....!");
+		
+	}
+	
+	
+	
+}
